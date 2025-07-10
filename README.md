ee72b3be928be8ed20caf8316506d4c1f12cc014
dwipaa98 <dwipaa98@gmail.com>
2025-07-10 16:36:51 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# adekkakakngentot
-adekkakakngentot
+# [adekkakakngentot](https://adekkakakngentot.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://adekkakakngentot.pages.dev';
+export const nama = 'Adek Kakak Ngentot';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
+export const terbit = '2025-01-25T00:01:21Z';
+
+export const google = 'y9IRGtnoAK58vpkHn0V8mv1hsvJaDFPu4dIKOeewwP4';
+export const yandex = '5ae18de2ccee24d0';
+
+export const index = 'd8495150-7c4e-4558-a2a7-e98989c6063a';
